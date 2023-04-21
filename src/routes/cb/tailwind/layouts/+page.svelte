6f57a502be56292c5d85a9cebbd88eeb062ddcb9
CaBrace <christopher.a.brace@gmail.com>
2023-04-21 01:16:27 -0400

    chore: restructure content

---
+++ src/routes/cb/tailwind/layouts/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../../../app.postcss';
+</script>
+
+<div class="flex flex-row h-screen w-50">
+  <div class="hover:h-screen bg-red-500 hover:w-screen hover:cursor-pointer">a</div>
+  <div class="grow-3 hover:h-screen hover:w-screen justify-end bg-green-500 hover:cursor-pointer ">b</div>
+  <div class="hover:h-screen hover:w-screen transition-width bg-blue-500 hover:cursor-pointer ">c</div>
+</div>
+
+<style>
+  
+</style>
+
+
